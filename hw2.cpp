1ef68b98441f7826ee6fcf131282e8bbc5af754d
GreenNeiben_AT <70635689+Neiben@users.noreply.github.com>
2021-03-06 09:28:29 +0300

    Add files via upload

--- hw2.cpp
+++ hw2.cpp
@@ -81,22 +81,34 @@ int main()
     vector <string> result;
     vector<string>operations;
     string tmp;
-    char oper;
+    string abc = " ";
+    char dot = '.';
     bool check;
     for (int i = 0; i < expression.length(); i++) {
         tmp += expression[i];
         if (isdigit(expression[i])) {
-            result.push_back(tmp);  
+            result.push_back(tmp);            
             tmp = "";           
         }
+        else if (tmp == ".") {
+            result.push_back(tmp);
+            tmp = "";
+        }
+        /*else if (isdigit(expression[i]) && (expression[i-1] == dot)) {
+            result.push_back(tmp);
+            result.push_back(abc);
+            tmp = "";
+        }*/
         else if (op(tmp)) {
             if ((operations.empty()) || (operations.back() == "("))
             {
+                result.push_back(abc);
                 operations.push_back(tmp);
                 tmp = "";
             }          
             else if (priority(operations.back()) < priority(tmp))
             {
+                result.push_back(abc);
                 operations.push_back(tmp); 
                 tmp = "";
             }
@@ -106,6 +118,7 @@ int main()
                     result.push_back(operations.back());
                     operations.pop_back();                   
                 }
+                result.push_back(abc);
                 operations.push_back(tmp);   
                 tmp = "";
             }
@@ -147,18 +160,70 @@ int main()
 
 
 
-    //start calculating
+   // start calculating
     vector<string> answer;
     string tmp3;
+    string separator = " ";
     for (int i = 0; i < tr_rez.size(); i++) {
         tmp3 += tr_rez[i];
         if (isdigit(tr_rez[i])) {
+            while (tr_rez[i] != ' ') {
+                answer.push_back(tmp3);
+            }           
+        }
+        if (tmp3 == abc) {
+            answer.push_back(abc);
+            tmp3 = "";
+        }
+
+        else if (op(tmp3)) {
+            for (int i = 0; i < answer.size(); i++) {
+                string A = answer.back();
+                answer.pop_back();
+                if (answer.back() == " ") {
+                    answer.pop_back();
+                }
+                string B = answer.back();
+                answer.pop_back();
+                answer.push_back(calculate(B, A, tmp3));
+
+                tmp3 = "";
+            }
+        }
+    }
+    cout << endl << " your answer is : " << answer.back();
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /* tmp3 += tr_rez[i];
+        if (isdigit(tr_rez[i])) {
+            answer.push_back(tmp3);
+            tmp3 = "";
+        }
+        else if (tr_rez[i] == dot) {
+            answer.push_back(tmp3);
+            tmp3 = "";
+        }
+        else if (tmp3 == abc) {
             answer.push_back(tmp3);
             tmp3 = "";
         }
         else if (op(tmp3)) {
             string A = answer.back();
             answer.pop_back();
+            if (answer.back() == " ") {
+                answer.pop_back();
+            }
             string B = answer.back();
             answer.pop_back();
             answer.push_back(calculate(B, A, tmp3));
@@ -166,7 +231,7 @@ int main()
             tmp3 = "";
         }
     }
-    cout << endl <<" your answer is : " << answer.back();
+    cout << endl <<" your answer is : " << answer.back();*/
 }
     
 
